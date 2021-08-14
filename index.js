91912570180b8ff74440a45720554829c796acf7
Hashem Gray <Hgray1109@gmail.com>
2021-08-14 16:54:16 -0400

    commit tree 4298b7f6c6d9552d8732092a39b29b295494d75f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Your code here
+function saturdayFun(activity) {
+    switch(activity){
+        case 'bathe my dog':
+            return `This Saturday, I want to ${activity}!`;
+            default:
+                return "This Saturday, I want to roller-skate!"
+    }
+  }
+  
+  function mondayWork(job) {
+    switch(job){
+        case 'work from home':
+            return `This Monday, I will ${job}.`;
+            default:
+                return "This Monday, I will go to the office."
+    }
+  
+  }
+  
+  function wrapAdjective(highlight) {
+    const savedHighlight = highlight === undefined? "*": highlight
+    return function(adjective) {
+        return "You are " + savedHighlight + adjective + savedHighlight + "!"
+    }
+  }
+  
